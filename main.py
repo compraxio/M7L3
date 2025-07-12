901356548ea4ec391cef864cf3836616486d1825
compraxio <luisangelacu10@gmail.com>
2025-07-12 14:52:26 -0500

    Mejorando

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ if camara:
     print("Hola")
 else:
     print("Adios")
+
+#git branch: te dice la rama en la que estas
+#git branch nombre: Crea una nueva rama
+#git checkout nombre rama: esto es para cambiar de rama
+#git branch -d nombre: elimina la rama siempre icuando ya este unida a la rama main
+#git branch -D nombre: elimina la rama sin importar nada
+#git marge nombre rama: para unir 2 ramas en la que se esta, con la que se selecciona
