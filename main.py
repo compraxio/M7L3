dfb600494152fde3f7dc360841f8f49945b869a6
compraxio <luisangelacu10@gmail.com>
2025-07-12 15:19:19 -0500

    Hola

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ else:
 #git checkout nombre rama: esto es para cambiar de rama
 #git branch -d nombre: elimina la rama siempre icuando ya este unida a la rama main
 #git branch -D nombre: elimina la rama sin importar nada
-#git marge nombre rama: para unir 2 ramas en la que se esta, con la que se selecciona
+#git merge nombre rama: para unir 2 ramas en la que se esta, con la que se selecciona
